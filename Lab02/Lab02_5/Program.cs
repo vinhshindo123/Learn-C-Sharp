d11a825769ce07fb078859d2c3bd9ab1bfb40401
vinhshindo123 <vinhnguyenquang71@gmail.com>
2025-04-19 21:52:05 +0700

    Upload Lab02

---
+++ Lab02/Lab02_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+void HoanDoi(ref int a, ref int b)
+{
+    int c = a;
+    a = b;
+    b = c;
+}
+
+int a, b;
+Console.Write("Nhập số nguyên a: ");
+a = int.Parse(Console.ReadLine());
+Console.Write("Nhập số nguyên b: ");
+b = int.Parse(Console.ReadLine());
+HoanDoi(ref a, ref b);
+Console.WriteLine($"Hai số a và b sau khi hoàn đổi là: a - {a}, b - {b}");
---
+++ Lab02/Lab02_6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+void SapXep(int[] a)
+{
+    int n = a.Length;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            if (a[i] > a[j])
+            {
+                int c = a[i];
+                a[i] = a[j];
+                a[j] = c;
+            }
+        }
+    }
+}
+
+int n;
+Console.Write("Nhập số phần tử của mảng: ");
+n = int.Parse(Console.ReadLine());
+int[] a = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Nhập phần tử thứ {i + 1}: ");
+    a[i] = int.Parse(Console.ReadLine());
+}
+
+SapXep(a);
+
+foreach (int i in a)
+{
+    Console.Write(i + " ");
+}
