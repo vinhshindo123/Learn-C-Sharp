99b9d7bcffbef40bf9e5a65cb3cfbc2efd164e1d
Vinhshindo <vinhshndo210720@gmail.com>
2025-05-11 21:54:37 +0700

    Upload Lab Tuan 5

---
+++ Tuan5/Lab/Lab/Controllers/UserController.cs
@@ -0,0 +1,81 @@
+﻿using Lab.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab.Controllers
+{
+    public class UserController : Controller
+    {
+        static List<User> users = new List<User>()
+        {
+             new User() {id="1",username="aaa",password="1",phone="111",email="111"},
+             new User() {id="2",username="bbb",password="1",phone="111",email="111"},
+             new User() {id="3",username="ccc",password="1",phone="111",email="111"},
+             new User() {id="4",username="ccc",password="1",phone="111",email="111"},
+             new User() {id="5",username="ddd",password="1",phone="111",email="111"},
+        };
+
+        public IActionResult Index()
+        {
+            return View(users);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(User user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    users.Add(user);
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex) 
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+            }
+            return View(user);
+        }
+        [HttpGet("User/Edit/{id}")]
+        public IActionResult Edit(string id)
+        {
+            var user = users.FirstOrDefault(u => u.id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+        [HttpPost]
+        public IActionResult Edit(User upUser)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var exUser = users.FirstOrDefault(u => u.id == upUser.id);
+                    if (exUser == null)
+                    {
+                        return NotFound();
+                    }
+
+                    exUser.username = upUser.username;
+                    exUser.password = upUser.password;
+                    exUser.phone = upUser.phone;
+                    exUser.email = upUser.email;
+
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+            }
+            return View(upUser);
+        }
+    }
+}
---
+++ Tuan5/Lab/Lab/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Không được để trống!")]
+        [Display(Name = "Mã sinh viên")]
+        public string id { get; set; }
+        [Display(Name = "Tài khoản")]
+        [Required(ErrorMessage = "Không được để trống!")]
+        [MaxLength(20, ErrorMessage = "Tài khoản từ 6 - 20 ký tự")]
+        [MinLength(6, ErrorMessage = "Tài khoản từ 6 - 20 ký tự")]
+        public string username { get; set; }
+        [Required(ErrorMessage = "Không được để trống!")]
+        [MaxLength(10, ErrorMessage = "Mật khẩu từ 6 - 10 ký tự")]
+        [MinLength(6, ErrorMessage = "Mật khẩu từ 6 - 10 ký tự")]
+        [Display(Name = "Mật khẩu")]
+        public string password { get; set; }
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Không được để trống!")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Sai định dạng email")]
+        public string email { get; set; }
+        [Display(Name = "Điện thoại")]
+        [Required(ErrorMessage = "Không được để trống!")]
+        public string phone { get; set; }
+
+    }
+}
